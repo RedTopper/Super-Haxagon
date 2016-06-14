d32867678e58b3d71d48bcc02f65a064dab9fe47
AJ Walter <aj.walter@aol.com>
2016-06-14 01:27:26 -0500

    Help... Me...
    Former-commit-id: 90e0484bb08e10b38fc0b74e5733be441733bc07

--- source/sound.c
+++ source/sound.c
@@ -2,43 +2,84 @@
 
 void audioLoad(const char *path, Track *sound, int channel) {
 	FILE *file = fopen(path, "rb");
-	if(file != NULL){
-		fseek(file, 0, SEEK_END);
-		sound->sndsize = ftell(file);
-		fseek(file, 0, SEEK_SET);
-		sound->sndsize = sound->sndsize - 0x48; //Skip wav header.
-		fseek(file, 0x48, SEEK_SET);
-		sound->sndbuffer = linearAlloc(sound->sndsize);
-		fread(sound->sndbuffer, 1, sound->sndsize, file);
-		fclose(file);
-		if (sound->sndbuffer != NULL) {
-            sound->loaded = true;
-			sound->channel = channel;
-		} else {
-            sound->loaded = false;
-		}
-	} else {
-        sound->loaded = false;
+	if(file == NULL){
+	    sound->loaded = false; //bad file
+		return;
 	}
+	
+	char sig[4];
+	fread(sig, 1, 4, file);
+	
+	if(sig[0] != 'R' && sig[1] != 'I' && sig[2] != 'F' && sig[3] != 'F'){
+		sound->loaded = false; //bad signature
+		fclose(file);
+		return;
+	}
+	
+	fseek(file, 40, SEEK_SET);
+	fread(&(sound->dataSize), 4, 1, file);
+	fseek(file, 22, SEEK_SET);
+	fread(&(sound->channels), 2, 1, file);
+	fseek(file, 24, SEEK_SET);
+	fread(&(sound->sampleRate), 4, 1, file);
+	fseek(file, 34, SEEK_SET);
+	fread(&(sound->bitsPerSample), 2, 1, file);
+	
+	if(sound->dataSize == 0 || (sound->channels != 1 && sound->channels != 2) || (sound->bitsPerSample != 8 && sound->bitsPerSample != 16)) {
+		sound->loaded = false; //bad header
+		fclose(file);
+		return;
+	}
+	
+	sound->data = linearAlloc(sound->dataSize);
+	
+	if(!(sound->data)) {
+		linearFree(sound->data); //could this cause problems?
+		sound->loaded = false; //bad buffer
+		fclose(file);
+		return;
+	}
+	
+	fseek(file, 44, SEEK_SET);
+	fread(sound->data, 1, sound->dataSize, file);
+	fclose(file);
+	
+	if(sound->bitsPerSample == 8) {
+		sound->ndspFormat = (sound->channels == 1) ? NDSP_FORMAT_MONO_PCM8  : NDSP_FORMAT_STEREO_PCM8 ;
+	} else {
+		sound->ndspFormat = (sound->channels == 1) ? NDSP_FORMAT_MONO_PCM16 : NDSP_FORMAT_STEREO_PCM16;
+	}
+	
+	ndspChnReset(channel);
+	ndspChnSetInterp(channel, NDSP_INTERP_NONE);
+	ndspChnSetRate(channel, sound->sampleRate);
+	ndspChnSetFormat(channel, sound->ndspFormat);
+	
+	sound->loaded = true;
+	sound->ndspChannel = channel;
 }
 
 void audioFree(Track *sound) {
 	if(sound->loaded) {
-		memset(sound->sndbuffer, 0, sound->sndsize);
-		GSPGPU_FlushDataCache(sound->sndbuffer, sound->sndsize);
-		linearFree(sound->sndbuffer);
+		memset(sound->data, 0, sound->dataSize);
+		linearFree(sound->data);
 	}
 }
 
 bool audioPlay(Track *sound, bool loop) {
 	if (sound->loaded) {
-		u32 flags;
-		if (loop) {
-			flags = SOUND_FORMAT_16BIT | SOUND_REPEAT;
-		} else {
-			flags = SOUND_FORMAT_16BIT;
-		}
-		csndPlaySound(sound->channel, flags, 44100, 1, 0, sound->sndbuffer, sound->sndbuffer, sound->sndsize);
+		ndspChnReset(sound->ndspChannel);
+		ndspWaveBuf waveBuf;
+		memset(&waveBuf, 0, sizeof(ndspWaveBuf)); //not sure if this is needed.
+		
+		waveBuf.data_vaddr = (u32 *)(sound->data);
+		waveBuf.nsamples = sound->dataSize / (sound->bitsPerSample >> 3); //wtf
+		waveBuf.looping = loop;
+		waveBuf.status = NDSP_WBUF_FREE;
+		
+		DSP_FlushDataCache(sound->data, sound->dataSize);
+		
+		ndspChnWaveBufAdd(sound->ndspChannel, &waveBuf);  ////IT CRASHES HERE
 		return true;
 	} else {
 		return false;
@@ -46,16 +87,11 @@ bool audioPlay(Track *sound, bool loop) {
 }
 
 void audioUnload() {
-	CSND_SetPlayState(SOUND_CHANNEL(8), 0);
-	csndExecCmds(true);
-	CSND_SetPlayState(SOUND_CHANNEL(9), 0);
-	csndExecCmds(true);
-	CSND_SetPlayState(SOUND_CHANNEL(10), 0);
-	csndExecCmds(true);
-	CSND_SetPlayState(SOUND_CHANNEL(11), 0);
-	csndExecCmds(true);
-	CSND_SetPlayState(SOUND_CHANNEL(12), 0);
-	csndExecCmds(true);
+	ndspChnWaveBufClear(1);
+	ndspChnWaveBufClear(2);
+	ndspChnWaveBufClear(3);
+	ndspChnWaveBufClear(4);
+	ndspChnWaveBufClear(5);
 
 	audioFree(&g_hexagon);
 	audioFree(&g_select);
@@ -65,31 +101,30 @@ void audioUnload() {
 }
 
 void initSounds() {
-	audioLoad("romfs:/sound/hexagon.bin", &g_hexagon, SOUND_CHANNEL(8));
-	audioLoad("romfs:/sound/select.bin", &g_select,SOUND_CHANNEL(9));
-	audioLoad("romfs:/sound/begin.bin", &g_begin, SOUND_CHANNEL(10));
-	audioLoad("romfs:/sound/over.bin", &g_over, SOUND_CHANNEL(11));
+	audioLoad("romfs:/sound/hexagon.bin", &g_hexagon, 1);
+	audioLoad("romfs:/sound/select.bin", &g_select, 2);
+	audioLoad("romfs:/sound/begin.bin", &g_begin, 3);
+	audioLoad("romfs:/sound/over.bin", &g_over, 4);
 	g_bgm.level = -1;
 }
 
 void playLevelBGM(int level) {
-	CSND_SetPlayState(SOUND_CHANNEL(12), 0);
-	csndExecCmds(true);
+	ndspChnReset(5);
 	if(g_bgm.level != level) {
 		audioFree(&g_bgm);
 		switch(level) {
 			case 0:
-				audioLoad("romfs:/music/drFinkelfracken.bin", &g_bgm, 12); break;
+				audioLoad("romfs:/music/drFinkelfracken.bin", &g_bgm, 5); break;
 			case 1:
-				audioLoad("romfs:/music/jackRussel.bin", &g_bgm, 12); break;
+				audioLoad("romfs:/music/jackRussel.bin", &g_bgm, 5); break;
 			case 2:
-				audioLoad("romfs:/music/callMeKatla.bin", &g_bgm, 12); break;
+				audioLoad("romfs:/music/callMeKatla.bin", &g_bgm, 5); break;
 			case 3:
-				audioLoad("romfs:/music/captainCool.bin", &g_bgm, 12); break;
+				audioLoad("romfs:/music/captainCool.bin", &g_bgm, 5); break;
 			case 4:
-				audioLoad("romfs:/music/commandoSteve.bin", &g_bgm, 12); break;
+				audioLoad("romfs:/music/commandoSteve.bin", &g_bgm, 5); break;
 			case 5:
-				audioLoad("romfs:/music/mazeOfMayonnaise.bin", &g_bgm, 12); break;
+				audioLoad("romfs:/music/mazeOfMayonnaise.bin", &g_bgm, 5); break;
 		}
 		g_bgm.level = level;
 	}

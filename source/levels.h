df0d7b331e0a4249996fe5071be732b6bc953a92
AJ Walter <aj.walter@aol.com>
2016-06-16 20:49:23 -0500

    Start creating a level editor (java)
    Former-commit-id: d9cdfe8bc02f0d1b888d6bba44883c81698b869b

--- source/levels.h
+++ source/levels.h
@@ -4,13 +4,7 @@
 
 #define TAU 6.28318530718
 #define TOTAL_LEVELS 6
-#define MAX_ELEMENTS_PER_PATTERN 20
-#define NUM_PATTERNS 2
-
-//These defines define the lookups in the arrays to human form.
-#define DISTANCE_FROM_CENTER 0
-#define SIDE 1
-#define LENGTH 2
+#define TOTAL_PATTERNS 2
 
 typedef struct {
 	double radians;
@@ -20,7 +14,15 @@ typedef struct {
 	double pulseSpeed;
 } LevelData;
 
+typedef struct {
+	int numberOfWalls;
+	int* distanceFromCenter;
+	int* side;
+	int* length;
+} Pattern;
+
 LevelData g_levelData[TOTAL_LEVELS];
 
+Pattern writePattern(int numberOfWalls, int* distanceFromCenter, int* side, int* length);
 void initLevelData();
 void resetLevelData();

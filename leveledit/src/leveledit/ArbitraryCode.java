3024a55a0187e74c215034a2ec8fdee3a30d8c58
AJ Walter <aj.walter@aol.com>
2016-06-17 13:28:13 -0500

    Allow the execution of arbitrary code in runtime.
    Former-commit-id: e6094929d2c3b8f96f1f30667bc6904dd0856259

---
+++ leveledit/src/leveledit/ArbitraryCode.java
@@ -0,0 +1,17 @@
+package leveledit;
+
+import java.util.ArrayList;
+
+public class ArbitraryCode {	
+	/**
+	 * You can write your own code here to generate patterns programmatically. 
+	 * Just return an array of patterns and the runtime will allow you to add 
+	 * it to the contents of the level patterns by typing r! */
+	public static ArrayList<Pattern> runCode() {
+		ArrayList<Pattern> patterns = new ArrayList<>();
+		
+		//Write code here
+		
+		return patterns;
+	}
+}

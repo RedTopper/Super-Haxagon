432b84cf0ea9d957ffdcfd547b7d8e738ae88ef9
AJ Walter <aj.walter@aol.com>
2016-06-19 17:09:38 -0500

    Use sf2dlib to draw triangles. This uses an experimental build of sf2dlib!
    Former-commit-id: 8e83491eb71bbe4396d60817c4cf8028bb31d08f

--- source/triangle.c
+++ source/triangle.c
@@ -1,84 +1,8 @@
 #include "triangle.h"
 
-void scanLine(int x1, int y1, int x2, int y2) {
-	int sx, sy, dx1, dy1, dx2, dy2, x, y, m, n, k, cnt;
-
-	sx = x2 - x1;
-	sy = y2 - y1;
-
-	if (sy < 0 || (sy == 0 && sx < 0)) {
-		k = x1; x1 = x2; x2 = k;
-		k = y1; y1 = y2; y2 = k;
-		sx = -sx;
-		sy = -sy;
-	}
-
-	if (sx > 0) dx1 = 1;
-	else if (sx < 0) dx1 = -1;
-	else dx1 = 0;
-
-	if (sy > 0) dy1 = 1;
-	else if (sy < 0) dy1 = -1;
-	else dy1 = 0;
-
-	m = ABS(sx);
-	n = ABS(sy);
-	dx2 = dx1;
-	dy2 = 0;
-
-	if (m < n) {
-		m = ABS(sy);
-		n = ABS(sx);
-		dx2 = 0;
-		dy2 = dy1;
-	}
-
-	x = x1; y = y1;
-	cnt = m + 1;
-	k = n / 2;
-
-	while (cnt-- > 0) {
-		if ((y >= 0) && (y < SCREEN_HEIGHT)) {
-			if (x < ContourX[y][0]) ContourX[y][0] = x;
-			if (x > ContourX[y][1]) ContourX[y][1] = x;
-		}
-
-		k += n;
-		if (k < m) {
-			x += dx2;
-			y += dy2;
-		} else {
-			k -= m;
-			x += dx1;
-			y += dy1;
-		}
-	}
-}
-
-void drawPoly(Point points[], int length) {
-	int SCREEN_WIDTH = (sf2d_get_current_screen() == GFX_TOP ? TOP_WIDTH : BOT_WIDTH);
-
-	for (int y = 0; y < SCREEN_HEIGHT; y++) {
-		ContourX[y][0] = INT_MAX; // We will min this, so set it max.
-		ContourX[y][1] = INT_MIN; // We will max this, so set it min.
-	}
-	
-	for(int num = 0; num < length; num++) {
-		if(num == length - 1) {
-			scanLine(points[num].x, points[num].y, points[0].x, points[0].y);
-		} else {
-			scanLine(points[num].x, points[num].y, points[num + 1].x, points[num + 1].y);
-		}
-	}
-
-	for (int y = 0; y < SCREEN_HEIGHT; y++) {
-		if (ContourX[y][1] >= ContourX[y][0]) {
-			int inverseY = SCREEN_HEIGHT - 1 - y;
-			if(ContourX[y][0] < 0) ContourX[y][0] = 0;
-			if(ContourX[y][1] < 0) ContourX[y][1] = 0;
-			if(ContourX[y][0] >= SCREEN_WIDTH) ContourX[y][0] = SCREEN_WIDTH - 1;
-			if(ContourX[y][1] >= SCREEN_WIDTH) ContourX[y][1] = SCREEN_WIDTH - 1;
-			sf2d_draw_line(ContourX[y][0], inverseY, ContourX[y][1], inverseY, 1, points[0].color);
-		}
-	}
+void drawTriangle(const Point points[3]) {
+	sf2d_draw_triangle(points[0].x, SCREEN_HEIGHT - 1 - points[0].y,
+					   points[1].x, SCREEN_HEIGHT - 1 - points[1].y,
+					   points[2].x, SCREEN_HEIGHT - 1 - points[2].y,
+					   points[0].color);
 }
--- source/triangle.h
+++ source/triangle.h
@@ -7,16 +7,6 @@
 #define SCREEN_HEIGHT 240
 #define TOP_WIDTH  400
 #define BOT_WIDTH  320
-#define ABS(x) ((x >= 0) ? x : -x)
-
-typedef struct {
-	int x1;
-	int y1;
-	int x2;
-	int y2;
-	int x3;
-	int y3;
-} Triangle;
 
 typedef struct
 {
@@ -24,15 +14,4 @@ typedef struct
   u32 color;
 } Point;
 
-long ContourX[SCREEN_HEIGHT][2];
-
-/*
- * Code borrowed from here:
- * http://stackoverflow.com/questions/11139724/i-need-a-pixel-perfect-triangle-fill-algorithm-to-avoid-aliasing-artifacts
- * 
- * Scans a side of a triangle setting min X and max X in ContourX[][]
- * (using the Bresenham's line drawing algorithm).
- * I have no idea what is happening here.
- */
-void scanLine(int x1, int y1, int x2, int y2);
-void drawPoly(Point points[], int length);
+void drawTriangle(const Point points[3]);

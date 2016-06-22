97e0e2cfe9811f25c1b202f1e6c372d03b2c79d4
flarn2006 <flarn2006@gmail.com>
2016-06-21 20:48:36 -0400

    Update font.cpp
    Former-commit-id: 45441dca4565becf43c603786af7e161573f5579

--- source/font.cpp
+++ source/font.cpp
@@ -1,23 +1,18 @@
 #include "font.h"
 
-BmpFont *font16 = NULL;
-BmpFont *font32 = NULL;
+BmpFont font16;
+BmpFont font32;
 
 void writeFont(Point p, const char* s, bool large) {
-	if(font16 == NULL) {
-		font16 = new BmpFont("romfs:/font16.bff"); //Load font file
+	if(!font16) {
+		font16.load("romfs:/font16.bff"); //Load font file
 	}
-	if(font32 == NULL) {
-		font32 = new BmpFont("romfs:/font32.bff"); //Load font file
+	if(!font32) {
+		font32.load("romfs:/font32.bff"); //Load font file
 	}
 	if(large) {
-		font32->drawStr(s, p.x, p.y, p.color);
+		font32.drawStr(s, p.x, p.y, p.color);
 	} else {
-		font16->drawStr(s, p.x, p.y, p.color);
+		font16.drawStr(s, p.x, p.y, p.color);
 	}
-}
-
-void freeFonts() {
-	font16->free();
-	font32->free();
 }

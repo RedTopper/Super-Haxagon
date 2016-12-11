1c34c014079964a9ece1c994713e00d11f55e3d7
AJ Walter <aj.walter@aol.com>
2016-12-11 01:50:14 -0600

    writing out file.
    Former-commit-id: 271fef35d671b0706f60b63a112e6bb4f2391b25

--- leveledit-two/src/parts/Pattern.java
+++ leveledit-two/src/parts/Pattern.java
@@ -1,5 +1,48 @@
 package parts;
 
-public class Pattern {
+import java.io.File;
+import java.io.IOException;
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
+import red.Util;
+
+public class Pattern {
+	public static final String HEADER = "PATTERN1.0";
+	public static final String FOOTER = "ENDPATTERN";
+	public static final String EXTENSION = ".ptn";
+	
+	private File file;
+	private ArrayList<Wall> walls = new ArrayList<>();
+	
+	public Pattern(File patternFile) throws IOException {
+		file = patternFile;
+		createPattern(Util.loadBinaryFile(patternFile));
+	}
+	
+	private void createPattern(ByteBuffer patternRawData) throws BufferUnderflowException, IOException {
+		Util.checkString(patternRawData, HEADER);
+		int numberOfWalls = patternRawData.getInt();
+		for(int i = 0; i < numberOfWalls; i++) {
+			walls.add(new Wall(patternRawData.getChar(), patternRawData.getChar(), patternRawData.getChar()));
+		}
+		Util.checkString(patternRawData, FOOTER);
+	}
+
+	public String getName() {
+		return file.getName();
+	}
+	
+	public void writeFile() {
+		
+	}
+	
+	public int getByteLength() {
+		return
+		HEADER.length() + 	//Size of the header
+		4 + 				//Integer to represent how many walls there are
+		Wall.BYTE_LENGTH +  //size of a wall
+		FOOTER.length(); 	//Size of the footer
+	}
 }

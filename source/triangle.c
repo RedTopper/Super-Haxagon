398d3c2fef932252b0600c4949f0813f5f4d31ce
Justin Wedeen <justinwedeen@aol.com>
2016-12-30 15:49:00 -0600

    Github is being wierd
    Getting rid of everything before merge, cause for some reason it wont
    merge.

--- source/triangle.c
+++
@@ -1,8 +0,0 @@
-#include "triangle.h"
-
-void drawTriangle(const Point points[3]) {
-	sf2d_draw_triangle(points[0].x, SCREEN_HEIGHT - 1 - points[0].y,
-					   points[1].x, SCREEN_HEIGHT - 1 - points[1].y,
-					   points[2].x, SCREEN_HEIGHT - 1 - points[2].y,
-					   points[0].color);
-}

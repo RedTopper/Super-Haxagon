a250eecf1632043054f50056f1871ab005a6d39f
AJ Walter <git@awalter.net>
2025-03-16 12:02:54 -0500

    Take 3

---
+++ cmake/functions/macos.cmake
@@ -0,0 +1,60 @@
+find_program(SIPS_EXE sips)
+find_program(ICONUTIL_EXE iconutil)
+
+function(generate_icns target)
+    cmake_parse_arguments(PARSE_ARGV 0 ICONUTIL_ICNS "" "ICON" "")
+
+    if (NOT SIPS_EXE OR NOT ICONUTIL_EXE)
+        message(WARNING "Could not find tools for making icons!")
+        return()
+    endif()
+
+    if (NOT DEFINED ICONUTIL_ICNS_ICON)
+        message(FATAL_ERROR "generate_icns: need to define location .png icon file")
+    endif()
+
+    set(outtarget "${target}_icns")
+
+    set(ICONSET_DIR "${CMAKE_CURRENT_BINARY_DIR}/${target}.iconset")
+    set(ICNS_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${target}.icns")
+
+    foreach(size IN ITEMS 16 32 64 128 256 512)
+        set(ICON_OUT "${ICONSET_DIR}/icon_${size}x${size}.png")
+        set(ICON_OUT2X "${ICONSET_DIR}/icon_${size}x${size}@2x.png")
+
+        math(EXPR size2x "${size} * 2")
+
+        add_custom_command(
+            OUTPUT "${ICON_OUT}"
+            COMMAND "${SIPS_EXE}" -z "${size}" "${size}" ${ICONUTIL_ICNS_ICON} --out "${ICON_OUT}"
+            DEPENDS "${ICONUTIL_ICNS_ICON}"
+            COMMENT "Creating icon ${size}x${size}"
+            VERBATIM
+        )
+
+        add_custom_command(
+            OUTPUT "${ICON_OUT2X}"
+            COMMAND "${SIPS_EXE}" -z "${size2x}" "${size2x}" ${ICONUTIL_ICNS_ICON} --out "${ICON_OUT2X}"
+            DEPENDS "${ICONUTIL_ICNS_ICON}"
+            COMMENT "Creating icon ${size}x${size}@2x"
+            VERBATIM
+        )
+
+        list(APPEND ICONS ${ICON_OUT})
+        list(APPEND ICONS ${ICON_OUT2X})
+    endforeach ()
+
+    add_custom_command(
+            OUTPUT "${ICNS_OUTPUT}"
+            COMMAND "${ICONUTIL_EXE}" -c icns -o "${ICNS_OUTPUT}" "${ICONSET_DIR}"
+            DEPENDS "${ICONS}"
+            COMMENT "Creating .icns"
+            VERBATIM
+    )
+
+    add_custom_target(${outtarget} DEPENDS "${ICNS_OUTPUT}")
+
+    add_dependencies(${target} ${outtarget})
+
+    set(MACOSX_BUNDLE_ICON_FILE "${ICNS_OUTPUT}" PARENT_SCOPE)
+endfunction()
--- cmake/macos.cmake
+++ cmake/macos.cmake
@@ -4,70 +4,31 @@ message(STATUS "Building for macOS...")
 
 set(PLATFORM_NAME "MACOS")
 
-set(DRIVER_PLATFORM source/Driver/macOS/PlatformMacOS.cpp source/Driver/Common/PlatformIsFullyRandom.cpp)
+set(DRIVER_PLATFORM
+    source/Driver/macOS/PlatformMacOS.cpp
+    source/Driver/Common/PlatformIsFullyRandom.cpp
+)
 
 # Prevent default install rules in release
 if (CMAKE_BUILD_TYPE STREQUAL "Release")
     set(BUILD_BUNDLE TRUE)
 endif()
 
+include(cmake/functions/macos.cmake)
+
 include(cmake/common/sfml.cmake)
 
 # Only make the bundle in Release mode
 if (BUILD_BUNDLE)
-    find_program(SIPS_EXE sips)
-    find_program(ICONUTIL_EXE iconutil)
-
-    if (SIPS_EXE AND ICONUTIL_EXE)
-        message(STATUS "Building macOS icons...")
-
-        set(ICNS_OUT "${CMAKE_CURRENT_BINARY_DIR}/SuperHaxagon.icns")
-        set(ICONSET_DIR "${CMAKE_CURRENT_BINARY_DIR}/SuperHaxagon.iconset")
-
-        foreach(size IN ITEMS 16 32 64 128 256 512)
-            set(ICON_IN "${CMAKE_CURRENT_SOURCE_DIR}/media/sources/icon-full.png")
-            set(ICON_OUT "${ICONSET_DIR}/icon_${size}x${size}.png")
-            set(ICON_OUT2X "${ICONSET_DIR}/icon_${size}x${size}@2x.png")
-
-            math(EXPR size2x "${size} * 2")
-
-            add_custom_command(
-                OUTPUT "${ICON_OUT}"
-                COMMAND "${SIPS_EXE}" -z "${size}" "${size}" ${ICON_IN} --out "${ICON_OUT}"
-                DEPENDS "${ICON_IN}"
-                COMMENT "Creating icon ${size}x${size}"
-                VERBATIM
-            )
-
-            add_custom_command(
-                OUTPUT "${ICON_OUT2X}"
-                COMMAND "${SIPS_EXE}" -z "${size2x}" "${size2x}" ${ICON_IN} --out "${ICON_OUT2X}"
-                DEPENDS "${ICON_IN}"
-                COMMENT "Creating icon ${size}x${size}@2x"
-                VERBATIM
-            )
-
-            list(APPEND ICONS ${ICON_OUT})
-            list(APPEND ICONS ${ICON_OUT2X})
-        endforeach ()
-
-        add_custom_command(
-                OUTPUT "${ICNS_OUT}"
-                COMMAND "${ICONUTIL_EXE}" -c icns -o "${ICNS_OUT}" "${ICONSET_DIR}"
-                DEPENDS "${ICONS}"
-                COMMENT "Creating icns"
-                VERBATIM
-        )
-
-        add_dependencies(SuperHaxagon "${ICNS_OUT}")
-        set(MACOSX_BUNDLE_ICON_FILE "${ICNS_OUT}")
-    endif()
-
     set(MACOSX_BUNDLE_BUNDLE_NAME ${APP_NAME})  # Short name
     set(MACOSX_BUNDLE_GUI_IDENTIFIER "net.awalter.${APP_NAME}")
     set(MACOSX_BUNDLE_BUNDLE_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
     set(MACOSX_BUNDLE_LONG_VERSION_STRING ${MACOSX_BUNDLE_BUNDLE_VERSION})
     set(MACOSX_BUNDLE_SHORT_VERSION_STRING ${MACOSX_BUNDLE_BUNDLE_VERSION})
+
+    generate_icns(SuperHaxagon
+        ICON "${CMAKE_CURRENT_SOURCE_DIR}/media/sources/icon-full.png"
+    )
 
     set_target_properties(SuperHaxagon PROPERTIES
         MACOSX_BUNDLE TRUE

d463ed62c1fb17eab4992a3b2b19c36ec5d6decf
AJ Walter <git@awalter.net>
2025-03-21 20:41:47 -0500

    Move Windows driver

---
+++ driver/Windows/README.md
@@ -0,0 +1,19 @@
+# Windows Driver
+
+This driver supports Windows with SFML2 as a backend.
+
+## Building
+
+Windows is (probably) the easiest platform to build for!
+
+1. Install Visual Studio https://visualstudio.microsoft.com/vs/
+2. Open the root folder
+3. Press "Play"
+
+You can probably also use the "Visual Studio Developer Command Prompt" to generate
+a Ninja or vcproj file directly, if needed, but I find just opening the folder to
+be the easiest.
+
+## License
+
+This driver follows the license scheme of the parent driver at `driver/SFML`.
---
+++ driver/Windows/windows.cmake
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2025 AJ Walter, see driver/SFML/README.md for MIT usage.
+# SPDX-License-Identifier: GPL-3.0-or-later OR MIT
+
+# cmake
+
+message(STATUS "Building for windows...")
+
+set(PLATFORM_NAME "WINDOWS")
+
+set(DRIVER_PLATFORM driver/Windows/source/PlatformWin.cpp)
+
+# SFML sets the warning level to /W4 and Visual Studio sets it to /W3.
+# Clear the default flags (for ALL targets) then add /W4 back to our target later.
+string(REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+string(REGEX REPLACE "/W[0-4]" "" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+
+include(driver/SFML/sfml.cmake)
+
+# GUI Entrypoint
+target_link_options(${PROJECT_NAME} PRIVATE "/SUBSYSTEM:WINDOWS" "/ENTRY:WinMainCRTStartup")
+
+target_compile_options(${PROJECT_NAME} PRIVATE "/W4")
+
+# Also distribute OpenAL with the built binary
+add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy ${SFML_SOURCE_DIR}/extlibs/bin/$<IF:$<EQUAL:${CMAKE_SIZEOF_VOID_P},8>,x64,x86>/openal32.dll $<TARGET_FILE_DIR:${PROJECT_NAME}>
+)
+
+install(FILES $<TARGET_FILE_DIR:${PROJECT_NAME}>/openal32.dll DESTINATION .)
